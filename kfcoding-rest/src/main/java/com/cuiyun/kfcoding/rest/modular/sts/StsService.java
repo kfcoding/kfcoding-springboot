0cc16d76b87c8022da16b541d5d54a21a3024f09
lytai2013@163.com <702431328@qq.com>
2018-05-21 14:35:52 +0800

    Merge branch 'master' of https://github.com/kfcoding/kfcoding

---
+++ kfcoding-rest/src/main/java/com/cuiyun/kfcoding/rest/modular/sts/StsService.java
@@ -0,0 +1,57 @@
+package com.cuiyun.kfcoding.rest.modular.sts;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+import com.aliyuncs.sts.model.v20150401.AssumeRoleRequest;
+import com.aliyuncs.sts.model.v20150401.AssumeRoleResponse;
+/**
+ * Created by carrie on 2018/5/20.
+ */
+public class StsService {
+    public static void main(String[] args) {
+        String endpoint = "sts.aliyuncs.com";
+        String accessKeyId = "LTAIw80sNhwIUe9p";
+        String accessKeySecret = "Scn0WaGWecj9QIPnHzfchOwvIwqryZ";
+        String roleArn = "acs:ram::1584087979028292:role/kfcoding-enduser";
+        String roleSessionName = "session-name";
+        String policy = "{\n" +
+                "    \"Version\": \"1\", \n" +
+                "    \"Statement\": [\n" +
+                "        {\n" +
+                "            \"Action\": [\n" +
+                "                \"oss:*\"\n" +
+                "            ], \n" +
+                "            \"Resource\": [\n" +
+                "                \"acs:oss:*:*:*\" \n" +
+                "            ], \n" +
+                "            \"Effect\": \"Allow\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}";
+        try {
+            // Init ACS Client
+            DefaultProfile.addEndpoint("", "", "Sts", endpoint);
+            IClientProfile profile = DefaultProfile.getProfile("", accessKeyId, accessKeySecret);
+            DefaultAcsClient client = new DefaultAcsClient(profile);
+            final AssumeRoleRequest request = new AssumeRoleRequest();
+            request.setMethod(MethodType.POST);
+            request.setRoleArn(roleArn);
+            request.setRoleSessionName(roleSessionName);
+            request.setPolicy(policy); // Optional
+            final AssumeRoleResponse response = client.getAcsResponse(request);
+            System.out.println("Expiration: " + response.getCredentials().getExpiration());
+            System.out.println("Access Key Id: " + response.getCredentials().getAccessKeyId());
+            System.out.println("Access Key Secret: " + response.getCredentials().getAccessKeySecret());
+            System.out.println("Security Token: " + response.getCredentials().getSecurityToken());
+            System.out.println("RequestId: " + response.getRequestId());
+        } catch (ClientException e) {
+            System.out.println("Failed：");
+            System.out.println("Error code: " + e.getErrCode());
+            System.out.println("Error message: " + e.getErrMsg());
+            System.out.println("RequestId: " + e.getRequestId());
+        }
+    }
+}
